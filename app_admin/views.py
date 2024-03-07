a985e5b4d99bf18a7e18c0a4524f0c47721065c3
lilyv <lily.veinberg@gmail.com>
2024-03-07 09:39:49 +0200

    07.03

--- app_admin/views.py
+++ app_admin/views.py
@@ -11,7 +11,7 @@ from django.contrib import messages
 from django.urls import reverse
 from django.views.generic.detail import SingleObjectMixin
 from django.http import HttpResponseRedirect, Http404
-from datetime import date
+from datetime import datetime, date
 from django.utils import timezone
 
 
@@ -136,11 +136,12 @@ class FoodMenuCreateView(ManagerRequiredMixin, CreateView):
         )
 
         return super().form_valid(form)
+    def get_form(self, *args, **kwargs):
+        form = super(FoodMenuCreateView, self).get_form(*args, **kwargs)
+        form.fields['date'].queryset = MenuHeadlines.objects.all().filter(date__gte=date.today())
+        return form
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['test'] = 'Proov'
-        return context
+
 
 
 class FoodMenuListView(ManagerRequiredMixin, ListView):
